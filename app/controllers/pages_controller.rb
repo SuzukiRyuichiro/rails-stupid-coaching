9867af084112ee588b435df40ffd518323e9b431
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-02-15 12:21:39 +0900

    now the coach is working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,18 @@ class PagesController < ApplicationController
 
   def ask
   end
+
+  def home
+  end
+
+  def answer
+    @coach_answer = ""
+    if params[:question] == 'I am going to work'
+      @coach_answer = 'good'
+    elsif params[:question].end_with?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
